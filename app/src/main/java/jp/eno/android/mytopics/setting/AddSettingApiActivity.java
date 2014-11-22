42811299f5d307463108bab7b7d9e21270c9bf29
Hiroto Kitamura <kirikonknow@gmail.com>
2014-11-23 03:46:34 +0900

    リスナー追加

--- app/src/main/java/jp/eno/android/mytopics/setting/AddSettingApiActivity.java
+++ app/src/main/java/jp/eno/android/mytopics/setting/AddSettingApiActivity.java
@@ -3,7 +3,10 @@ package jp.eno.android.mytopics.setting;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 import android.view.WindowManager;
+import android.widget.EditText;
 
 import jp.eno.android.mytopics.R;
 
@@ -12,12 +15,22 @@ import jp.eno.android.mytopics.R;
  */
 public class AddSettingApiActivity extends Activity {
 
+    private EditText mEditText;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_setting_api);
 
         getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_ALWAYS_VISIBLE);
+
+        mEditText = (EditText) findViewById(R.id.add_setting_api_edit_text);
+
+        findViewById(R.id.add_setting_api_button_positive)
+                .setOnClickListener(createPositiveButtonCLickListener());
+
+        findViewById(R.id.add_setting_api_button_negative)
+                .setOnClickListener(createNegativeButtonClickListener());
     }
 
     /**
@@ -28,5 +41,23 @@ public class AddSettingApiActivity extends Activity {
     public static void start(Activity activity) {
         final Intent intent = new Intent(activity, AddSettingApiActivity.class);
         activity.startActivity(intent);
+    }
+
+    private View.OnClickListener createPositiveButtonCLickListener() {
+        return new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.d("AAAAA", mEditText.getText().toString());
+            }
+        };
+    }
+
+    private View.OnClickListener createNegativeButtonClickListener() {
+        return new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        };
     }
 }
