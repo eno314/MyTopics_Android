c103b49401c4ffe616415b2b44ff4eeb9f64345c
Hiroto Kitamura <kirikonknow@gmail.com>
2014-11-23 01:07:36 +0900

    タブのライブラリ追加

--- app/src/main/java/jp/eno/android/mytopics/MainActivity.java
+++ app/src/main/java/jp/eno/android/mytopics/MainActivity.java
@@ -1,5 +1,6 @@
 package jp.eno.android.mytopics;
 
+import android.app.ActionBar;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
 import android.support.v4.app.FragmentManager;
@@ -16,6 +17,9 @@ public class MainActivity extends FragmentActivity {
     protected void onCreate( Bundle savedInstanceState ) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final ActionBar actionBar = getActionBar();
+        actionBar.setNavigationMode(ActionBar.NAVIGATION_MODE_TABS);
 
         final FragmentManager fragmentManager = getSupportFragmentManager();
         if (savedInstanceState == null) {
