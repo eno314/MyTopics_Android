a84659754423c1db9c22be862c194bd0319552ec
Hiroto Kitamura <kirikonknow@gmail.com>
2014-11-23 04:05:51 +0900

    入力されたAPIを実行する

--- app/src/main/java/jp/eno/android/mytopics/setting/AddSettingApiActivity.java
+++ app/src/main/java/jp/eno/android/mytopics/setting/AddSettingApiActivity.java
@@ -3,17 +3,27 @@ package jp.eno.android.mytopics.setting;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
+import android.text.TextUtils;
 import android.view.View;
 import android.view.WindowManager;
 import android.widget.EditText;
+import android.widget.Toast;
+
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
 
 import jp.eno.android.mytopics.R;
+import jp.eno.android.mytopicslibrary.model.ApiList;
+import jp.eno.android.mytopicslibrary.request.ApiListRequest;
+import jp.eno.android.mytopicslibrary.volley.VolleyQueue;
 
 /**
  * Created by eno314 on 2014/11/17.
  */
 public class AddSettingApiActivity extends Activity {
+
+    private static final String MESSAGE_TEXT_EMPTY = "URLを入力後に決定ボタンを押してください";
+    private static final String MESSAGE_FAILED_REQUEST = "APIリクエストに失敗しました。通信状態や入力値を確認してください";
 
     private EditText mEditText;
 
@@ -35,21 +45,11 @@ public class AddSettingApiActivity extends Activity {
                 .setOnClickListener(createNegativeButtonClickListener());
     }
 
-    /**
-     * ApiListActivityに遷移するためのメソッド
-     *
-     * @param activity 起動元のアクティビティ
-     */
-    public static void start(Activity activity) {
-        final Intent intent = new Intent(activity, AddSettingApiActivity.class);
-        activity.startActivity(intent);
-    }
-
     private View.OnClickListener createPositiveButtonCLickListener() {
         return new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Log.d("AAAAA", mEditText.getText().toString());
+                onClickPositiveButton();
             }
         };
     }
@@ -61,5 +61,50 @@ public class AddSettingApiActivity extends Activity {
                 finish();
             }
         };
+    }
+
+    /**
+     * 決定ボタンをおした時の処理
+     */
+    private void onClickPositiveButton() {
+        final String editedText = mEditText.getText().toString();
+
+        if (TextUtils.isEmpty(editedText)) {
+            showMessage(MESSAGE_TEXT_EMPTY);
+            return;
+        }
+
+        VolleyQueue.getQueue(getApplicationContext()).add(buildRequest(editedText));
+    }
+
+    private ApiListRequest buildRequest(String url) {
+        return new ApiListRequest.Builder(url)
+                .setListener(new Response.Listener<ApiList>() {
+                    @Override
+                    public void onResponse(ApiList response) {
+                        showMessage(response.name);
+                    }
+                })
+                .setErrorListener(new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        showMessage(MESSAGE_FAILED_REQUEST);
+                    }
+                })
+                .build();
+    }
+
+    private void showMessage(String message) {
+        Toast.makeText(getApplicationContext(), message, Toast.LENGTH_SHORT).show();
+    }
+
+    /**
+     * ApiListActivityに遷移するためのメソッド
+     *
+     * @param activity 起動元のアクティビティ
+     */
+    public static void start(Activity activity) {
+        final Intent intent = new Intent(activity, AddSettingApiActivity.class);
+        activity.startActivity(intent);
     }
 }
